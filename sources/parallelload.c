3abc7cc93db83c4a8c7d8133a0103f3514da3cdf
O seok KWON <ohseok158@gmail.com>
2016-07-06 16:05:58 +0900

    Alpha
    implement insert, find, travel B-tree
    implement prefetch

--- sources/parallelload.c
+++ sources/parallelload.c
@@ -1,9 +1,13 @@
 #include "parallelload.h"
 
 static b_tree* b_root;
+static int last_hash;
 
 void* do_posix_fadvise(void* data) {
-	printf("Prefetch\n");
+	fdata* will_load = (fdata*)data;
+	int fd = open(will_load->path, O_RDONLY);
+	posix_fadvise(fd, will_load->offset, will_load->length, POSIX_FADV_WILLNEED);
+	close(fd);
 	pthread_exit((void*)0);
 }
 
@@ -17,6 +21,13 @@ void* do_load(void* args) {
 	// Find targets by args
 	// IF Find; do_posix_fadvise
 	// Else; pthread_exit();
+	fdata* data = b_find_f(b_root, *(int*)args);
+	if( data != NULL){
+		// using thread or not
+		int fd = open(data->path, O_RDONLY);
+		posix_fadvise(fd, data->offset, data->length, POSIX_FADV_WILLNEED);
+		close(fd);
+	}
 	pthread_exit(0);
 }
 
@@ -33,25 +44,20 @@ void* do_prepare(void* args) {
 		memset(filepath, '\0', 255);
 		size_t offset;
 		size_t length;
-		sscanf(line,"%[^,],%zd,%zd", filepath, &offset, &length);
+		sscanf(line, "%[^,],%zd,%zd", filepath, &offset, &length);
 		// printf("%s, %zd, %zd\n", filepath, offset, length);
-		
+
 		int key = hash(filepath);
 		fdata* newf = new_fdata(filepath, offset, length);
-		if(b_root == NULL){
-			b_init(b_root);
-		}
-		/*
-			Make B-tree [ key : path, offset, length ]
-		*/
-		// if (b_root != NULL){
 
-		// }
-		// else{
-			// b_root->key = key;
-			// b_root->data = newf;
-		// }
+		// printf("%s, %zd, %zd\n", newf->path, newf->offset, newf->length);
+		b_tree* b_temp = b_init(key, newf);
+		if (b_root == NULL) {
+			b_root = b_temp;
+		}
+		b_insert(b_root, b_temp);
 	}
+	// b_travel(b_root);
 	free(line);
 	fclose(stream);
 	pthread_exit(0);
@@ -65,6 +71,7 @@ int __libc_start_main(int (*main) (int, char **, char **), int argc, char **ubp_
 
 	/* Do my things here */
 	b_root = NULL;
+	last_hash = 0;
 	pthread_t start_main_thread;
 	if ( pthread_create(&start_main_thread, NULL, do_prepare, (void*)0) < 0) {
 		perror("Error : read pthread_create");
@@ -75,21 +82,29 @@ int __libc_start_main(int (*main) (int, char **, char **), int argc, char **ubp_
 	return original__libc_start_main(main, argc, ubp_av, init, fini, rtld_fini, stack_end);
 }
 
-ssize_t read(int fildes, void *buf, size_t nbyte) {
-	ssize_t (*original_read)(int fildes, void *buf, size_t nbyte);
+ssize_t read(int fd, void *buf, size_t count) {
+	ssize_t (*original_read)(int fd, void *buf, size_t count);
 	ssize_t ret;
-	// pthread_t read_thread;
-	// if ( pthread_create(&read_thread, NULL, do_hi, (void*)0) < 0) {
-	// 	perror("Error : read pthread_create");
-	// }
-	// pthread_join(read_thread, NULL);
-	/* 
-		if last == hash_key
-			NULL
-		else
-			do_prefetch
-	*/
+
+	int pid = getpid();
+	char procpath[255];
+	char fpath[255];
+	memset(procpath, '\0', 255);
+	memset(fpath, '\0', 255);
+	sprintf(procpath, "/proc/%d/fd/%d", pid, fd);
+	if (readlink(procpath, fpath, 255) < 0) {
+		perror("readlink Error");
+	}
+	int my_key = hash(fpath);
+	if (last_hash != my_key) {
+		last_hash = my_key;
+		pthread_t load_thread;
+		if ( pthread_create(&load_thread, NULL, do_load, (void*)&my_key ) < 0) {
+			perror("Error : pthread_create ( load_thread )");
+		}
+		// pthread_join(load_thread, NULL);
+	}
 	original_read = dlsym(RTLD_NEXT, "read");
-	ret = (*original_read)(fildes, buf, nbyte);
+	ret = (*original_read)(fd, buf, count);
 	return ret;
 }
--- sources/parallelload.h
+++ sources/parallelload.h
@@ -31,36 +31,74 @@ fdata* new_fdata(char* path, size_t offset, size_t length) {
 	newfdata->length = length;
 	return newfdata;
 }
-typedef struct b_tree{
+typedef struct b_tree {
 	int key;
 	struct b_tree* left;
 	struct b_tree* right;
 	void* data;
 } b_tree;
 
-void b_init(b_tree* bt){
+b_tree* b_init(int key, fdata* data) {
 	b_tree* newb = (b_tree*)malloc(sizeof(b_tree));
-	newb->key = 0ULL;
+	newb->key = key;
 	newb->left = NULL;
 	newb->right = NULL;
-	newb->data = NULL;
-	// return newb;
+	newb->data = (void*) data;
+	return newb;
 }
 
-void b_insert(b_tree* root, b_tree* newb){
+void b_insert(b_tree* root, b_tree* newb) {
 	int key = newb->key;
-	b_tree* temp = root;
-	while(temp == NULL){
-		if(temp->key == key){
-			/* temp->data->offset, length */
+	b_tree* me = root;
+	while (me != NULL) {
+		if (me->key == key) {
+			/* if Hole; may cause bugs */
+			if ( ((fdata*)(me->data))->offset + ((fdata*)(me->data))->length == ((fdata*)(newb->data))->offset) {
+				((fdata*)(me->data))->length += ((fdata*)(newb->data))->length;
+			}
+			return;
 		}
-		else if(temp->key > key){
-			temp = temp->left;
+		else if (me->key > key) {
+			// printf("%p -> %p\n", me, me->left);
+			if ( me->left == NULL) {
+				me->left = newb;
+				return;
+			}
+			me = me->left;
 		}
-		else{
-			temp = temp->right;
+		else {
+			if ( me->right == NULL) {
+				me->right = newb;
+				return;
+			}
+			me = me->right;
 		}
 	}
+}
+
+fdata* b_find_f(b_tree* root, int key){
+	b_tree* me = root;
+	while( me != NULL){
+		if( me->key == key ){
+			return (fdata*)(me->data);
+		}
+		else if( me->key > key){
+			me = me->left;
+		}
+		else {
+			me = me->right;
+		}
+	}
+	return NULL;
+}
+
+void b_travel(b_tree* root) {
+	if (root != NULL) {
+		printf("%s, %zu, %zu\n", ((fdata*)(root->data))->path, ((fdata*)(root->data))->offset, ((fdata*)(root->data))->length);
+		b_travel(root->left);
+		b_travel(root->right);
+	}
+	// printf("NULL\n");
 }
 
 typedef union {
